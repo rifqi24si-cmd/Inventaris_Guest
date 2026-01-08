99073b4c7a69002b8545537b35ccd869f795145c
rifqi24si_cmd <rifqi24si@mahasiswa.pcr.ac.id>
2026-01-09 04:51:11 +0700

    prubahan

--- resources/views/layouts/guest/app.blade.php
+++ resources/views/layouts/guest/app.blade.php
@@ -7,7 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
 
-    <title>Lugx Gaming Shop HTML5 Template</title>
+    <title>Inventaris</title>
 
     @include('layouts.guest.css')
 
